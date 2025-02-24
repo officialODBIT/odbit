720fdc58acb4ad30dcf6b19ea83386a8b51e6817
officialODBIT <contact@odbit.org>
2025-02-23 22:23:51 -0500

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -12,3 +12,19 @@ window.onload = function() {
     document.getElementById("cookie-banner").style.display = "none";
   }
 };
+// Create a useless cookie
+function storeCookie() {
+    const cookieName = "UserData";
+    const cookieValue = "This cookie does absolutely nothing!";
+    const expirationDays = 7;
+
+    const date = new Date();
+    date.setTime(date.getTime() + (expirationDays * 24 * 60 * 60 * 1000)); // Set expiration
+    const expires = "expires=" + date.toUTCString();
+
+    document.cookie = `${cookieName}=${cookieValue}; ${expires}; path=/`;
+    console.log("A useless cookie has been created!");
+}
+
+// Call the function to create the cookie
+createUselessCookie();

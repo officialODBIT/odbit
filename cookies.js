2cb1594c5ef7b3bd63d9d5a055965ac1baae69ef
officialODBIT <contact@odbit.org>
2025-02-23 20:33:12 -0500

    Create cookies.js

---
+++ cookies.js
@@ -0,0 +1,12 @@
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Set a non-usable cookie
+setCookie("testCookie", "non-usable-value", 7);
